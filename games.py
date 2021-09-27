328d44b0d7fd4ea1e147b213ab53f0c4d8b56796
konstantinosTabakis <konstantinos_tabakis@yahoo.com>
2021-09-27 11:48:27 +0300

    update commit

--- games.py
+++ games.py
@@ -8,7 +8,6 @@ import datetime
 now=datetime.datetime.now()
 
 
-
 page= requests.get("https://www.skroutz.gr/c/1413/playstation_4_games.html")
 soup=BeautifulSoup(page.content,"html.parser")
 games=[]
@@ -16,13 +15,14 @@ games=[]
 for game in soup.find_all("a",attrs={"class":"js-sku-link"}):
     if game.text !="":
         games.append(game.text)
-
-
-for i in range(1, len(games),2):
+index=0
+if len(games[0])>len(games[1]):
+    index=1
+for i in range(index, len(games),2):
     games[i]= games[i][3:8]
+
 date=str(now.day)  +"_"+ str(now.hour)+ "_"+ str(now.minute) 
 f=open( f"{date}_games.txt","w+")
-f.write(date + "\n")
-for i in range(0,len(games),2):
-    f.writelines(f'game : {games[i]} - price : {games[i+1]} \n')
+for i in range(0,len(games)-1,2):
+    f.writelines(f'game : {games[i]} -  {games[i+1]} \n')
 f.close()
